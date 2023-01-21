4365f5333b833f51ed11cab87a6ed0c766e08f3e
msnitish <msnitish@gmail.com>
2023-01-22 02:25:20 +0900

    create board app

--- message_board/message_board/settings.py
+++ message_board/message_board/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'messages',
     'rest_framework',
+    'board',
 ]
 
 MIDDLEWARE = [
