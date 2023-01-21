5a7b90794ec73b120feb3584c0e2b700b9def3ca
msnitish <msnitish@gmail.com>
2023-01-22 02:27:55 +0900

    create Message model

--- message_board/board/models.py
+++ message_board/board/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Message(models.Model):
+    text = models.TextField()
+    source = models.GenericIPAddressField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
