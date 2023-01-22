c3de7bdb1d4dff348ed43385df384c0d0514f2ce
msnitish <msnitish@gmail.com>
2023-01-22 10:11:14 +0900

    register model and add serializer for model

---
+++ message_board/board/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import Message
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ('id', 'text', 'source', 'created_at')
